26c3066ff9d2e8e9c948409b891596b7293f636d
leiderY2K <leiderre14@gmail.com.com>
2024-05-28 12:54:04 -0500

    se agrego el login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,50 +1,61 @@
-import React, { useState } from 'react';
-import { TextField, Button, Checkbox, FormControlLabel, Box } from '@mui/material';
+import React from 'react';
+import { TextField, Button, Checkbox, FormControlLabel, Box, Card, CardContent, Typography } from '@mui/material';
 import { useForm } from 'react-hook-form';
-
-
+import { Link as RouterLink } from 'react-router-dom';
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
 
-    function onSubmit(date) {
-        console.log(date);
-        let nombre_completo = date.nombre + date.apellido
-        console.log(nombre_completo);
-    }
+  const onSubmit = (data) => {
+    console.log(data);
+    const nombreCompleto = `${data.nombre} ${data.apellido}`;
+    console.log(nombreCompleto);
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', margin: '20px auto', width: '50%' }}>
+  return (
+    <div className="bg-slate-800 border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="60vh">
+        <Card sx={{ maxWidth: 700, padding: 4 }}>
+          <CardContent>
+            <Typography variant="h5" component="div" gutterBottom justifyContent="font-bold text-center">
+              Inicio de sesión
+            </Typography>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                 <TextField
-                    id="email"
-                    label="Correo"
-                    variant="standard"
-                    fullWidth
-                    sx={{ marginBottom: 2 }}
+                  {...register('email')}
+                  id="email"
+                  label="Correo"
+                  variant="outlined"
+                  fullWidth
+                  sx={{ marginBottom: 2 }}
                 />
                 <TextField
-                    id="password"
-                    label="Contraseña"
-                    type="password"
-                    variant="standard"
-                    fullWidth
-                    sx={{ marginBottom: 2 }}
+                  {...register('password')}
+                  id="password"
+                  label="Contraseña"
+                  type="password"
+                  variant="outlined"
+                  fullWidth
+                  sx={{ marginBottom: 2 }}
                 />
                 <FormControlLabel
-                    control={<Checkbox />}
-                    label="Recordar contraseña"
-                    sx={{ color: 'text.primary' }}
+                  control={<Checkbox {...register('rememberPassword')} />}
+                  label="Recordar contraseña"
+                  sx={{ marginBottom: 2 }}
                 />
-                <Button
-                    variant="contained"
-                    size="large"
-                    sx={{ width: 200, margin: '20px auto' }}
-                    type="submit"
-                >
-                    Ingresar
+                <Button variant="contained" size="large" type="submit">
+                  Ingresar
                 </Button>
-            </Box>
-        </form>
-    );
-}
+                <RouterLink component={RouterLink} to="/" variant="body2" align="center">
+                  Volver a la página de inicio
+                </RouterLink>
+              </Box>
+            </form>
+          </CardContent>
+        </Card>
+      </Box>
+    </div>
+  );
+};
 export default Login;
